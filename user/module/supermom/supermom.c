06e1644a2059dbf33d0b412e62027eae6e3d1089
bateman <zz2700@columbia.edu>
2020-10-11 04:06:15 -0400

    part5

--- user/module/supermom/supermom.c
+++ user/module/supermom/supermom.c
@@ -2,9 +2,12 @@
 #include <linux/printk.h>
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
+#include <linux/debugfs.h>
 
 #define MODULE_NAME "Supermom"
 
+struct dentry *slog;
+int count;
 extern long (*func_ptr)(pid_t pid, uid_t __user *uid);
 
 long helper(pid_t pid, uid_t __user *uid)
@@ -25,8 +28,10 @@ long helper(pid_t pid, uid_t __user *uid)
 		if (copy_to_user(uid, &euid, sizeof(uid_t)))
 			return -EFAULT;
 
-	if (euid == 0)
+	if (euid == 0) {
+		count++;
 		return 0;
+	}
 	else
 		return -EACCES;
 }
@@ -35,12 +40,17 @@ int simple_init(void)
 {
 	printk(KERN_INFO "Loading %s\n", MODULE_NAME);
 	func_ptr = helper;
+	if ((slog = debugfs_create_dir("superlog", NULL)) == NULL)
+		printk(KERN_WARNING "debugfs_create_dir() failed\n");
+	count = 0;
+	debugfs_create_u32("success", 0644, slog, &count);
 	return 0;
 }
 
 void simple_exit(void)
 {
 	printk(KERN_INFO "Removing %s\n", MODULE_NAME);
+	debugfs_remove_recursive(slog);
 }
 
 module_init(simple_init);
